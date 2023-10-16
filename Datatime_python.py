22d655406a51227db22fcd09d2e87a066882838b
P@rashmgr <96672535+devilthami@users.noreply.github.com>
2023-10-16 21:46:02 +0545

    several data time list

---
+++ Datatime_python.py
@@ -0,0 +1,21 @@
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+print(x.strftime("%m"))
+print(x.strftime("%w"))
+print(x.strftime("%B"))
+print(x.strftime("%b"))
+print(x.strftime("%y"))
+print(x.strftime("%Y"))
+print(x.strftime("%H"))
+print(x.strftime("%I"))
+print(x.strftime("%p"))
+print(x.strftime("%M"))
+print(x.strftime("%S"))
+print(x.strftime("%f"))
+print(x.strftime("%z"))
+print(x.strftime("%Z"))
+print(x.strftime("%j"))
